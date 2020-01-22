783930f029ec088bcabb20c66a9cfb69886247c8
vickyvamja <56025558+vickyvamja@users.noreply.github.com>
2020-01-22 12:55:06 +0530

    Add files via upload

--- prog3.c
+++ prog3.c
@@ -1,70 +1,57 @@
 #include<stdio.h>
-struct stud
-{
-	int rollno;
-	char name[25];
-	int mark[20];
-	int total;
-};
-void add(struct stud *s,int );
-void display(struct stud *s,int ,int);
 int main()
 {
 	int n;
-	int res;
-	printf("howmany record insert\n");
+	printf("\n enter the number of element : \n");
 	scanf("%d",&n);
-	struct stud s[n];
-	add(s,n);
-}
-void add(struct stud *s,int n)
-{
-	int m;
+	int arr[n];
 	for(int i=0;i<n;i++)
 	{
-		printf("\nrollno :");
-		scanf("%d",&s[i].rollno);
-		printf("\nname :");
-		scanf("%s",s[i].name);
-		printf("enter the howmany mark");
-		scanf("%d",&m);
-		s[i].total=0;
-		for(int j=0;j<m;j++)
+		scanf("%d",&arr[i]);
+	}
+	/*for(int i=0;i<n-1;i++)
+	{
+		if(i%2==0)
 		{
-			printf("\nmark :");
-			scanf("%d",&s[i].mark[j]);
-			s[i].total=s[i].total+s[i].mark[j];
+			if(arr[i]>arr[i+1])
+			{
+				int temp=arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]=temp;
+			}
+		}
+		else
+		{
+			if(arr[i]<arr[i+1])
+			{
+				int temp=arr[i];
+				arr[i]=arr[i+1];
+				arr[i+1]=temp;
+			}	
+		}
+	}*/
+
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(arr[i]<arr[j])
+			{
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
 		}
 	}
-	display(s,n,m);
-}
-void display(struct stud *s,int n,int m)
-{	
-	int flag;
+	for(int i=1;i<n;i++)
+	{
+		int temp=arr[i];
+		arr[i]=arr[i+1];
+		arr[i+1]=arr[i];
+	}
+
 	for(int i=0;i<n;i++)
 	{
-		printf("\nrollno: %d",s[i].rollno);
-		printf("\nname  : %s",s[i].name);
-		for(int j=0;j<m;j++){
-		printf("\nmark  : %d",s[i].mark[j]);
-		if(s[i].mark[j]<12)
-		{
-			flag=1;			
-		}
-		else
-		{
-			flag=2;
-		}
-		}
-		if(flag==1)
-		{
-			printf("\nresult is fail\n");
-		}
-		else
-		{
-			
-			printf("\ntotal  : %d",s[i].total);
-			printf("\nresult is pass\n");
-		}
+		printf("%d\n",arr[i]);
 	}
 }
